25cd8afdfede8a522f8d0f08ee4678a2e9c46a4b
SunDwarf <l@veriny.tf>
2017-01-21 14:19:25 +0000

    Allow changing what object is returned from Command instances.

--- curious/commands/__init__.py
+++ curious/commands/__init__.py
@@ -6,16 +6,18 @@ import functools
 from curious.commands.command import Command
 
 
-def command(*args, **kwargs):
+def command(*args, klass: type=Command, **kwargs):
     """
     A decorator to mark a function as a command.
     This will put a `factory` attribute on the function, which can later be called to create the Command instance.
 
     All arguments are passed to the Command class.
+
+    :param klass: The command class type to wrap the object in.
     """
 
     def __inner(func):
-        factory = functools.partial(Command, func, *args, **kwargs)
+        factory = functools.partial(klass, func, *args, **kwargs)
         func.factory = factory
         return func
 
