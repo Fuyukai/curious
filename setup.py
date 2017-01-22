c209ec1d98a85917c629c0dc259884da30d03f00
Laura <l@veriny.tf>
2017-01-22 16:57:16 +0000

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='discord-curious',
-    version='0.1.0',
+    version='0.1.1',
     packages=['curious', 'curious.http', 'curious.commands', 'curious.dataclasses', 'curious.ext'],
     url='https://github.com/SunDwarf/curious',
     license='MIT',
