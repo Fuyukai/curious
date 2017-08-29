30594b70de3d6b028cfcc52bdc3fa004ea03bfa3
Laura <l@veriny.tf>
2017-08-29 16:10:18 +0100

    Fix afk channel id crash
    Signed-off-by: Laura <l@veriny.tf>

--- curious/core/state.py
+++ curious/core/state.py
@@ -774,7 +774,7 @@ class State(object):
         guild._splash_hash = event_data.get("splash")
         guild.region = event_data.get("region")
         guild.mfa_level = event_data.get("mfa_level")
-        guild._afk_channel_id = int(event_data.get("afk_channel", 0))
+        guild.afk_channel_id = int(event_data.get("afk_channel", 0))
         guild.afk_timeout = event_data.get("afk_timeout")
         guild.verification_level = event_data.get("verification_level")
         guild._owner_id = int(event_data.get("owner_id", 0))
