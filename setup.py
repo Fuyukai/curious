b6e326f6bfe76243022f5bf6100e3dc3cd5215a9
Laura F. D <l@veriny.tf>
2018-01-26 23:54:24 +0000

    Change setup.py to backport certain things.
    Signed-off-by: Laura F. D <l@veriny.tf>

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
-import os
+import sys
 from pathlib import Path
 
 from setuptools import setup
 
-# Override readthedocs.
-on_rtd = os.environ.get('READTHEDOCS') == 'True'
-if on_rtd:
-    import pip
-    pip.main("install git+https://github.com/sphinx-doc/sphinx.git".split())
+install_requires = [
+        "asyncwebsockets>=0.2.0,<0.3.0",
+        "curio>=0.9.0,<0.10.0",
+        "pylru==1.0.9",
+        "oauthlib>=2.0.2,<2.1.0",
+        "pytz>=2017.3",
+        "asks>=1.3.0,<1.4.0",
+        "multidict>=2.1.6<2.2.0",
+        "multio>=0.2.0,<0.3.0"
+]
+
+# PEP 557
+if sys.version_info[0:2] <= (3, 6):
+    install_requires.append("dataclasses>=0.3")
+
+# TODO: PEP 567 backport
+# TODO: asynccontextmanager backport
+
 
 setup(
     name='discord-curious',
@@ -26,16 +39,7 @@ setup(
     setup_requires=[
         "setuptools_scm",
     ],
-    install_requires=[
-        "asyncwebsockets>=0.1.1",
-        "curio>=0.9.0,<0.10.0",
-        "pylru==1.0.9",
-        "oauthlib>=2.0.2,<2.1.0",
-        "pytz>=2017.3",
-        "asks>=1.3.0,<1.4.0",
-        "multidict>=2.1.6",
-        "multio>=0.1.0",
-    ],
+    install_requires=install_requires,
     extras_require={
         "voice": ["opuslib==1.1.0",
                   "PyNaCL==1.0.1"],
