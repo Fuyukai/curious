6f4fd51f478e5263ae4ba5ac708e819ce416f16f
TildeBeta <tildebetabot@gmail.com>
2018-06-06 21:00:58 +0300

    Add permissions based conditions

--- curious/commands/conditions.py
+++ curious/commands/conditions.py
@@ -3,7 +3,9 @@ Commonly used conditions.
 
 .. currentmodule:: curious.commands.conditions
 """
+from typing import Union, List
 
+from curious import Member, User
 from curious.commands import Context, condition
 
 
@@ -18,3 +20,25 @@ def is_owner():
         return ctx.author.id == owner.id
 
     return condition(_condition)
+
+
+def author_has_permissions(bypass_owner=True, **permissions):
+    def _condition(ctx: Context):
+        perms = ctx.channel.permissions(ctx.author)
+        return all(getattr(perms, name, None) == value
+                   for name, value in permissions.items())
+
+    return condition(_condition, bypass_owner=bypass_owner)
+
+
+def bot_has_permissions(bypass_owner=False, **permissions):
+    def _condition(ctx: Context):
+        perms = ctx.channel.me_permissions
+        return all(getattr(perms, name, None) == value
+                   for name, value in permissions.items())
+
+    return condition(_condition, bypass_owner=bypass_owner)
+
+
+author_has_perms = author_has_permissions
+bot_has_perms = bot_has_permissions
