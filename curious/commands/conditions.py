139a201b1d3ea59e2d3ad8b63b6dc176a4ff80ad
TildeBeta <tildebetabot@gmail.com>
2018-06-10 17:49:47 +0300

    Add docstrings to the conditions

--- curious/commands/conditions.py
+++ curious/commands/conditions.py
@@ -8,6 +8,19 @@ from curious.commands import Context, condition
 
 
 def is_owner():
+    """
+    A :func:`.condition` that ensures the author of the message is the owner of the bot.
+
+    The owner is checked automatically by using the application info of the bot.
+
+    Example::
+
+        @command()
+        @is_owner()
+        async def kill(ctx: Context):
+            await ctx.bot.kill()
+    """
+
     def _condition(ctx: Context):
         # If the application info request has not been completed
         # yet we cannot guarantee the command could be ran.
@@ -20,7 +33,25 @@ def is_owner():
     return condition(_condition)
 
 
-def author_has_permissions(bypass_owner=True, **permissions):
+def author_has_permissions(bypass_owner: bool = True, **permissions):
+    """
+    A :func:`.condition` that ensures the author of the
+    message has all of the specified permissions.
+
+    Example::
+
+        @command()
+        @author_has_permissions(kick_members=True)
+        async def kick(ctx: Context, member: Member):
+            await member.kick()
+            await ctx.channel.messages.send(':wave:')
+
+    :param bypass_owner:
+        Determines if the owner of the bot can run the command
+        regardless of if the condition failed or not.
+    :param permissions: A mapping of permissions to check.
+    """
+
     def _condition(ctx: Context):
         perms = ctx.channel.permissions(ctx.author)
         return all(getattr(perms, name, None) == value
@@ -29,7 +60,24 @@ def author_has_permissions(bypass_owner=True, **permissions):
     return condition(_condition, bypass_owner=bypass_owner)
 
 
-def bot_has_permissions(bypass_owner=False, **permissions):
+def bot_has_permissions(bypass_owner: bool = False, **permissions):
+    """
+    A :func:`.condition` that ensures the bot
+    has all of the specified permissions.
+
+    Example::
+
+        @command()
+        @bot_has_permissions(send_messages=True)
+        async def test(ctx: Context):
+            await ctx.channel.messages.send('The bot can send messages.')
+
+    :param bypass_owner:
+        Determines if the owner of the bot can run the command
+        regardless of if the condition failed or not.
+    :param permissions: A mapping of permissions to check.
+    """
+
     def _condition(ctx: Context):
         perms = ctx.channel.me_permissions
         return all(getattr(perms, name, None) == value
@@ -42,7 +90,25 @@ author_has_perms = author_has_permissions
 bot_has_perms = bot_has_permissions
 
 
-def author_has_roles(*roles, bypass_owner=True):
+def author_has_roles(*roles: str, bypass_owner: bool = True):
+    """
+    A :func:`.condition` that ensures the author of the message has all of the specified roles.
+
+    The role names must be exact matches.
+
+    Example::
+
+        @command()
+        @author_has_roles('Cool')
+        async def cool(ctx: Context):
+            await ctx.channel.messages.send('You are cool.')
+
+    :param roles: A collection of role names.
+    :param bypass_owner:
+        Determines if the owner of the bot can run the command
+        regardless of if the condition failed or not.
+    """
+
     def _condition(ctx: Context):
         if ctx.guild is None:
             return False
@@ -53,7 +119,25 @@ def author_has_roles(*roles, bypass_owner=True):
     return condition(_condition, bypass_owner=bypass_owner)
 
 
-def bot_has_roles(*roles, bypass_owner=False):
+def bot_has_roles(*roles: str, bypass_owner: bool = False):
+    """
+    A :func:`.condition` that ensures the bot has all of the specified roles.
+
+    The role names must be exact matches.
+
+    Example::
+
+        @command()
+        @bot_has_roles('Cool')
+        async def cool(ctx: Context):
+            await ctx.channel.messages.send('The bot is cool.')
+
+    :param roles: A collection of role names.
+    :param bypass_owner:
+        Determines if the owner of the bot can run the command
+        regardless of if the condition failed or not.
+    """
+
     def _condition(ctx: Context):
         if ctx.guild is None:
             return False
@@ -64,7 +148,22 @@ def bot_has_roles(*roles, bypass_owner=False):
     return condition(_condition, bypass_owner=bypass_owner)
 
 
-def is_guild_owner(bypass_owner=True):
+def is_guild_owner(bypass_owner: bool = True):
+    """
+    A :func:`.condition` that ensures the author of the message is also the owner of the guild.
+
+    Example::
+
+        @command()
+        @is_guild_owner('Cool')
+        async def test(ctx: Context):
+            await ctx.channel.messages.send('You are the owner of this guild.')
+
+    :param bypass_owner:
+        Determines if the owner of the bot can run the command
+        regardless of if the condition failed or not.
+    """
+
     def _condition(ctx: Context):
         if ctx.guild is None:
             return False
